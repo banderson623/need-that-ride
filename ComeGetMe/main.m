f328ffb396ea962e1cb280a72979d4bcee5ec25d
Brian Anderson <banderson623@gmail.com>
2012-07-16 23:56:51 -0500

    more changes

--- ComeGetMe/main.m
+++ ComeGetMe/main.m
@@ -8,11 +8,11 @@
 
 #import <UIKit/UIKit.h>
 
-#import "HelloWorldAppDelegate.h"
+#import "NeedThatRideAppDelegate.h"
 
 int main(int argc, char *argv[])
 {
     @autoreleasepool {
-        return UIApplicationMain(argc, argv, nil, NSStringFromClass([HelloWorldAppDelegate class]));
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([NeedThatRideAppDelegate class]));
     }
 }
